8a87ad144fbf594e171500a33d26bbb6c5683f6e
github username <github email address>
2019-11-27 00:45:38 -0500

    new account posting working

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,6 +1,29 @@
 module Api
     module V1
         class AccountsController < ApplicationController
+            def create
+                account = Account.new({username: params["account"]["username"]})
+                if account.save
+                    c = params["character"]
+                    character = {
+                        name: c["name"],
+                        profile_image: c["profile_image"],
+                        server: c["server"],
+                        api_id: c["account_id"],
+                        account_id: account.id,
+                        primary: true
+                    }
+                    character = Character.new(character) 
+                    if character.save
+                        render json: { valid: true } 
+                    else
+                        render json: { valid: false }
+                    end
+                else
+                    render json: { valid: false } 
+                end             
+            end
+
             def validate
                 if account = Account.all.find{|account|account.username == params["name"]}
                     render json: { valid: true, account: account, communities: account.communities, characters: account.characters }
